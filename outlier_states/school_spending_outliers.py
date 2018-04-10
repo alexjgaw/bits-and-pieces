e9819ec013c7b0df41a3bee8535f19bf99a8dc30
Alex Gaw <alexanderjgaw@gmail.com>
2018-04-09 23:41:26 -0500

    minor improvements consistency and concision

--- outlier_states/school_spending_outliers.py
+++ outlier_states/school_spending_outliers.py
@@ -4,10 +4,10 @@ filename = "Public School K-12 Education Spending Per Pupil by State 2014.txt"
 
 states = pandas.read_csv(filename, sep='\t', usecols=range(2), skiprows=[1])
 
-mean = int(states["Total"].mean())
-stdev = int(states["Total"].std())
+mean = states["Total"].mean()
+stdev = states["Total"].std()
 
-outlier = abs(states['Total'] - mean) > stdev
+outlier = abs(states["Total"] - mean) > stdev
 outlier_states = states[outlier]
 
 print(states)
